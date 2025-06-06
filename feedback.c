e8fef7005ea9f421d325c764b67e117100f0e4b0
mariaeduardaurbano <maria.eduardaoliveiraurbano@gmail.com>
2025-06-06 14:45:28 -0300

    Add files via upload

---
+++ feedback.c
@@ -0,0 +1,21 @@
+// Implemente um sistema com for para registrar as notas de atendimento de 7 clientes (de 0 a 10).
+// Ao final, calcule a média das notas. 
+// Use if para exibir se a média foi boa (>=8), razoável (>=5) ou ruim.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    float nota[7], media = 0.0, soma = 0.0;
+    for(int i = 0; i < 7; i++){
+        system("clear");
+        do{
+        printf("Caro Cliente, por favor dê uma nota ao nosso estabelecimento(1-7)\n");
+        scanf("%f", &nota[i]);
+        soma += nota[i];
+        } while(nota[i] < 1 || nota[i] > 7);
+        
+    }
+    media = soma / 7;
+    printf("A média de notas é: %.2f", media);
+    return 0;
+}
